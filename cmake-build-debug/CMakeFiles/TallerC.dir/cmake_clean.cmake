7615ccbc7686d7f5a525044d88e20d41ea96fe5e
Jonathan G Araya <jonathana1196@gmail.com>
2019-05-30 01:58:32 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TallerC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TallerC.dir/main.c.obj"
+  "TallerC.pdb"
+  "TallerC.exe"
+  "TallerC.exe.manifest"
+  "libTallerC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TallerC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
